602de715785b7e661aa27f9f9bf5a0e1a219f158
Jordan Boesch <jordan@boedesign.com>
2011-09-03 15:51:10 -0600

    changing config

--- config.php
+++ config.php
@@ -3,5 +3,5 @@ define('PSIGATE_CID', '');
 define('PSIGATE_USER_ID', '');
 define('PSIGATE_PASSWORD', '');
 define('PSIGATE_URL', 'https://dev.psigate.com:8645/Messenger/AMMessenger');
-define('PSIGATE_DISABLE_SSL', true);
+define('PSIGATE_DISABLE_SSL', false);
 ?>
